888b1d00897a4b20106b4709d0eb35b854d42a9c
DanielMafra12345 <daniel.mafra.pinto@escola.pr.gov.br>
2025-11-03 10:04:11 -0300

    Implement word game with user interaction
    Add a word game that asks for the user's name, age, and capital of Brazil.

--- jogo.py
+++ jogo.py
@@ -1,1 +1,19 @@
+locals=Brasilia
+print("Buenos dias mis amigos")
 
+#Descrição
+print("Jogo das Palavras")
+print("Responda as Perguntas Corretamente")
+#Perguntas
+nome=input("Qual é seu nome?")
+idade=input("Qual a sua idade?")
+print("Meu nome é", nome,"e minha idade é", idade,"anos")
+
+#If e else
+capital=input("Qual é a capital do Brasil?")
+if capital == Brasilia:
+    print("Correto!")
+else:
+    print("A Resposta Está Errada!")
+
+#Jogo Das Perguntas
